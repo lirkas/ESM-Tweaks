3e651bdcde2bb591df42c3b09da083b85a8def9e
lirkas <47765829+lirkas@users.noreply.github.com>
2025-09-28 06:10:18 +0200

    fix missing event
    and extra additional event fixes

--- src/main/java/lirkas/esmtweaks/event/handler/EntityEventHandler.java
+++ src/main/java/lirkas/esmtweaks/event/handler/EntityEventHandler.java
@@ -1,21 +1,23 @@
 package lirkas.esmtweaks.event.handler;
 
 import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.ai.EntityAITasks.EntityAITaskEntry;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import funwayguy.epicsiegemod.api.ITaskAddition;
 import funwayguy.epicsiegemod.api.TaskRegistry;
+import funwayguy.epicsiegemod.handlers.MainHandler;
 
 import lirkas.esmtweaks.ESMTweaks;
 import lirkas.esmtweaks.ai.addition.DiggingAITaskAddition;
 import lirkas.esmtweaks.config.ModConfig;
+import lirkas.esmtweaks.util.EntityUtil;
 import lirkas.esmtweaks.util.Util;
 
 /**
@@ -27,24 +29,43 @@ public class EntityEventHandler {
     public void onEntityDeath(LivingDeathEvent event) {
         
         // check if this is needed here or not
-        // if(event.getWorld().isRemote) {
-        //     return;
-        // }
+        if(event.getEntity().world.isRemote) {
+            return;
+        }
         
         if(ModConfig.AI.updateAITaskOnDeath && EntityLiving.class.isInstance(event.getEntityLiving())) {
 
             EntityLiving entityLiving = (EntityLiving) event.getEntityLiving();
-            ESMTweaks.logger.debug("updating AITask on death for " + entityLiving.getDisplayName());
+            ESMTweaks.logger.debug("updating AITask on death for " + entityLiving.getName());
             
-            //testing
-            ESMTweaks.logger.debug("AI List for " + entityLiving.getDisplayName());
-            for(EntityAITaskEntry taskEntry : entityLiving.tasks.taskEntries) {
-                ESMTweaks.logger.debug("    "+taskEntry.action.getClass()+"    ");
-            }
-
             // this allows for AI tasks to reset when the mob dies but it could cause unexpected issues 
             // with other mobs since it targets all mobs that have tasks attached to them.
             entityLiving.tasks.onUpdateTasks();
+        }
+    }
+
+    @SubscribeEvent
+    public void onEntityConstruct(EntityJoinWorldEvent event) {
+        
+        if(event.getWorld().isRemote) {
+            return;
+        }
+
+        // only doing things if this is an EntityLiving
+        if(!EntityLiving.class.isInstance(event.getEntity())) {
+            return;
+        }
+
+        EntityLiving entityLiving = (EntityLiving) event.getEntity();
+
+        ESMTweaks.logger.debug("onEntityConstruct " + entityLiving.getName());
+
+        // should not be called if ESM onEntityConstruct is still registered as a listener
+        new MainHandler().onEntityConstruct(event);
+
+        ESMTweaks.logger.debug("Tasks : ");
+        for(String taskName : EntityUtil.getAITasks(entityLiving, true)) {
+            ESMTweaks.logger.debug("    " + taskName);
         }
     }
 
@@ -53,18 +74,20 @@ public class EntityEventHandler {
      */
     @SubscribeEvent
     public void onEntityLivingSpawn(LivingSpawnEvent.SpecialSpawn event) {
+        
+        // only doing things if this is an EntityLiving
+        if(!EntityLiving.class.isInstance(event.getEntityLiving())) {
+            return;
+        }
+
+        ESMTweaks.logger.debug("onEntityConstruct "  + event.getEntity().getName());
+
+        EntityLiving entityLiving = (EntityLiving) event.getEntityLiving();
 
         ESMTweaks.logger.debug(event.getEntityLiving().getName() + " has spawned at" + 
             " x: " + event.getEntityLiving().getPosition().getX() +
             " y: " + event.getEntityLiving().getPosition().getY() +
             " z: " + event.getEntityLiving().getPosition().getZ());
-
-        // only doing things if this is an EntityEvent
-        if(!EntityLiving.class.isInstance(event.getEntityLiving())) {
-            return;
-        }
-
-        EntityLiving entityLiving = (EntityLiving) event.getEntityLiving();
 
         // going through each task in the registry to see if one in there
         // matches the DiggingAI and if the mob is capable to receive it.
