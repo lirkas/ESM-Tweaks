e7a585c9d001716cf3154ac2ca1ed309dd7946f5
lirkas <47765829+lirkas@users.noreply.github.com>
2025-09-28 03:06:59 +0200

    added new util class

---
+++ src/main/java/lirkas/esmtweaks/util/EntityUtil.java
@@ -0,0 +1,32 @@
+package lirkas.esmtweaks.util;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.ai.EntityAITasks.EntityAITaskEntry;
+
+
+public class EntityUtil {
+    
+    /**
+     * Gets all AI task names assigned to this entity.
+     * 
+     * @param entityLiving The entity to get AI tasks from.
+     * @param fullName If true, the full classpath of the AI class is shown, else only the name.
+     * @return A set containing names of the this entity AI Tasks.
+     */
+    public static Set<String> getAITasks(EntityLiving entityLiving, boolean fullName) {
+
+        Set<String> taskNames = new LinkedHashSet<String>();
+
+        for(EntityAITaskEntry taskEntry : entityLiving.tasks.taskEntries) {
+            taskNames.add(fullName ? 
+                taskEntry.action.getClass().getName() : 
+                taskEntry.action.getClass().getSimpleName()
+            );
+        }
+
+        return taskNames;
+    } 
+}
